58ad5f19e1265c14b1c6aef5edc92fbee5d4ea1d
ajhenri <amandajohe@live.com>
2019-12-31 17:46:51 -0500

    init ma extension, add logging, refactor initialization of DB, teardown appcontext handlr

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,36 @@
 import os
+import sys
+import logging
 
 from flask import Flask
 from flask_restplus import Api, Resource, fields
 
-from app.core.db import DBConnection
+from app.extensions import ma
+from app.core.db import init_db
 from app.resources import API_NAMESPACES
+
+# Simple logging instantiation.
+LOG_FILENAME = './logs/trading-simulator.log'
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(LOG_FILENAME),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('app.cfg', silent=True)
     
-    DBConnection.init_app(app)
+    init_db(app)
     
     api = Api(app, version='1.0', title='Trading Simulator API',
         description='A simple API developed for the stock trading simulation application of the same name.',
     )
+    ma.init_app(app)
 
     for ns in API_NAMESPACES:
         api.add_namespace(*ns)
@@ -25,5 +41,10 @@ def create_app():
     def test():
         counter = redis.incr("counter")
         return "<b>Count:</b> {}".format(counter)
+
+    @app.teardown_appcontext
+    def shutdown_session(exception=None):
+        from app.core.db import Session
+        Session.remove()
 
     return app
