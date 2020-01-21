60ecd62570eb9c769282c1eeaf486ed74ec1f93a
ajhenri <amandajohe@live.com>
2020-01-20 21:05:50 -0500

    account and position models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Column, ForeignKey, Integer, String, Binary, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, Binary, DateTime, Numeric
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base, declared_attr
 
@@ -30,6 +30,7 @@ class User(Base):
     first_name = Column(String(50))
     last_name = Column(String(50))
     client = relationship("OauthClient", uselist=False, back_populates="user")
+    account = relationship('Account', uselist=False, backref='users')
 
 class OauthScope(Base):
     __tablename__ = 'oauth_scopes'
@@ -65,3 +66,32 @@ class OauthAuthorizationCode(Base, OauthGrantMixin):
     __tablename__ = 'oauth_codes'
     code = Column(String(40), primary_key=True)
     redirect_uri = Column(String(2048), nullable=False)
+
+class Account(Base):
+    __tablename__ = 'accounts'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    cash_amount = Column(
+        Numeric(precision=19, scale=2, asdecimal=False, decimal_return_scale=None), 
+        default=0.00)
+    equity_amount = Column(
+        Numeric(precision=19, scale=2, asdecimal=False, decimal_return_scale=None), 
+        default=0.00)
+    initial_amount = Column(
+        Numeric(precision=19, scale=2, asdecimal=False, decimal_return_scale=None), 
+        default=0.00)
+    positions = relationship('Position')
+
+class Position(Base):
+    __tablename__ = 'positions'
+    id = Column(Integer, primary_key=True)
+    account_id = Column(Integer, ForeignKey('accounts.id'))
+    bought_at = Column(
+        Numeric(precision=19, scale=2, asdecimal=False, decimal_return_scale=None), 
+        nullable=False)
+    bought_on = Column(DateTime, nullable=False)
+    cost = Column(
+        Numeric(precision=19, scale=2, asdecimal=False, decimal_return_scale=None), 
+        nullable=False)
+    number_of_shares = Column(Integer, nullable=False)
+    ticker = Column(String(5), unique=True, nullable=False)
