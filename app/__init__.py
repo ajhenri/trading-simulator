104ec122ebe5689e02acc13d451655561fb43c04
ajhenri <amandajohe@live.com>
2020-02-04 20:01:55 -0500

    better logging handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,24 @@
 import os
 import sys
 import logging
+import traceback
 
 from flask import Flask
+from werkzeug.exceptions import HTTPException
 from flask_restplus import Api, Resource, fields
 
+from app.lib import errors
 from app.extensions import ma
 from app.database import init_db
 from app.resources import API_NAMESPACES
 
-# Simple logging instantiation.
-LOG_FILENAME = './logs/trading-simulator.log'
-
+# Simple logging setup
+file_handler = logging.handlers.RotatingFileHandler('./logs/trading-simulator.log', maxBytes=10000)
 logging.basicConfig(
+    level=logging.DEBUG,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler(LOG_FILENAME),
+        file_handler,
         logging.StreamHandler(sys.stdout)
     ]
 )
@@ -23,7 +26,7 @@ logging.basicConfig(
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py', silent=True)
-    
+
     init_db(app)
     
     api = Api(app, version='1.0', title='Trading Simulator API',
@@ -45,5 +48,18 @@ def create_app():
     def shutdown_session(exception=None):
         from app.database import Session
         Session.remove()
+
+    @app.errorhandler(Exception)
+    def handle_exception(e):
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        logging.error(str(e))
+        logging.error('\n' + ''.join(traceback.format_tb(exc_traceback)))
+
+        if isinstance(e, HTTPException):
+            response = { 'error': e.description }
+            return response, e.code
+        
+        response = { 'error': errors.DEFAULT }
+        return response, 500
 
     return app
