d29de487a5ac53a2bb9fd98e0d4526532de5a19c
ajhenri <amandajohe@live.com>
2020-02-17 15:23:12 -0500

    add base front-end component, setup webpack and node

---
+++ trader/frontend/js/Trader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Trader extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Trader</h1>
+            </div>
+        )
+    };
+}
